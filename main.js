d91e6e1ae29c37c30177076a013f24a233380e20
zc1415926 <zc1415926@126.com>
2022-01-12 16:14:52 +0800

    把devtools放在一个单独的窗口中并与主窗口分开显示，以免对主窗口中的内容产生干扰

--- main.js
+++ main.js
@@ -1,18 +1,27 @@
 const { app, BrowserWindow } = require('electron')
 const isDev = require('electron-is-dev');
 
+let devtools = null
+
 function createWindow() {
     const win = new BrowserWindow({
         width: 800,
         height: 600,
+        x: 100,
+        y: 0,
         webPreferences: {
-            nodeIntegration: true
+          contextIsolation: false,
+          nodeIntegration: true
         }
     })
-    // 这里对官网代码进行了修改，因为cra是启动了web服务而非静态文件
-    // win.loadFile('index.html');
+    //开发时Electron窗口调用'http://localhost:3000'中React动态构建的文件
+    //打包后Electron就调用生成的静态React文件
     win.loadURL(isDev ? 'http://localhost:3000' : `file://${__dirname}/build/index.html`);
-    //win.webContents.openDevTools();
+    
+    //把devtools放在一个单独的窗口中并与主窗口分开显示，以免对主窗口中的内容产生干扰
+    devtools = new BrowserWindow({x:1000, y:0})
+    win.webContents.setDevToolsWebContents(devtools.webContents)
+    win.webContents.openDevTools({ mode: 'detach' ,activate: 'false'})
 }
 
 app.whenReady().then(createWindow)
